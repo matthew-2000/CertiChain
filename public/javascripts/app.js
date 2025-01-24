a03109b8241219c8c168f77edb2b4a038b987ec1
matthew-2000 <m.ercolino1@studenti.unisa.it>
2025-01-24 12:23:12 +0100

    Improved UI

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,20 @@
-document.getElementById('connectBtn').addEventListener('click', handleConnect);
-document.getElementById('issueCertificateBtn').addEventListener('click', handleIssueCertificate);
-document.getElementById('checkCertificateBtn').addEventListener('click', handleCheckCertificate);
-document.getElementById('revokeCertificateBtn').addEventListener('click', handleRevokeCertificate);
-document.getElementById('getNFTsBtn').addEventListener('click', handleGetNFTs);
+document.addEventListener('DOMContentLoaded', () => {
+    const currentPage = window.location.pathname;
+
+    if (currentPage.includes('user.html')) {
+        document.getElementById('getNFTsBtn').addEventListener('click', handleGetNFTs);
+    }
+
+    if (currentPage.includes('admin.html')) {
+        document.getElementById('issueCertificateBtn').addEventListener('click', handleIssueCertificate);
+        document.getElementById('revokeCertificateBtn').addEventListener('click', handleRevokeCertificate);
+    }
+
+    if (currentPage.includes('verifier.html')) {
+        document.getElementById('checkCertificateBtn').addEventListener('click', handleCheckCertificate);
+    }
+
+    if (document.getElementById('connectBtn')) {
+        document.getElementById('connectBtn').addEventListener('click', handleConnect);
+    }
+});
